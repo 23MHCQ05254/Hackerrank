cbaf59c54cd611ce5142d6737856fa0ff9fdc7dc
Joseph J <jangajoseph77@gmail.com>
2025-09-13 18:09:53 +0530

    Second largest element in array

--- Arrays/HourGlass_Sum.java
+++ Arrays/HourGlass_Sum.java
@@ -39,7 +39,7 @@ class HourglassSum {
 
 }
 
-public class Solution {
+public class Hourglass_Sum {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
